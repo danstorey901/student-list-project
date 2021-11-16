0fb346100cebee2738f6e3ba06814bac0495bed7
Dan Storey <danstorey901@gmail.com>
2021-11-16 13:12:51 -0500

    magical angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  styles: ['h1 {color: red};']
 })
-export class AppComponent {
-  title = 'student-list-project';
+export class AppComponent implements OnInit {
+  title = 'Default Student-List';
+  name: string = "";
+  clicked(): void {
+    this.title = "Fred's Student-List";
+  }
+  clicked2(): void {
+    this.title = "Dan's Student-List";
+  }
+
+  ngOnInit(): void {
+
+    console.debug("This is a debug message");
+  }
 }
